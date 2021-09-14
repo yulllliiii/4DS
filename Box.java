1dd66e5b861cd713b1132809e52f27a5bf9e69de
gwakjusung <demontick@naver.com>
2021-09-14 17:27:56 +0900

    210914 box 파일 수정

---
+++ Box.java
@@ -0,0 +1,14 @@
+package ch13;
+
+public class Box {
+	private Object obj;
+	
+	public void setObject(Object obj) {
+		this.obj= obj;
+	}
+	
+	public Object getObject() {
+		return obj;
+	}
+}
+dskkdf
